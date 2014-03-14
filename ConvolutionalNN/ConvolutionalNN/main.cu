fa8ccd4b3798c20b6a60628d373d1392b5dd5a30
Xeverus <michal.wendel@gmail.com>
2014-03-14 15:02:47 +0100

    dodalem tutorial, wczytuje sie juz fotka

--- ConvolutionalNN/ConvolutionalNN/main.cu
+++ ConvolutionalNN/ConvolutionalNN/main.cu
@@ -3,18 +3,24 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
-
-#include "Utils/Utils.h"
-#include "Kernels.h"
+#include <fstream>
+#include <iostream>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include "Utils/Utils.h"
+#include "Kernels.h"
+
 
 int main()
 {
-	cv::Mat a;
+	cv::Mat rgb = cv::imread("data/test.png", CV_LOAD_IMAGE_COLOR);
+	cv::namedWindow("some name", CV_WINDOW_AUTOSIZE);
+	cv::imshow("some name", rgb);
 
+	cvWaitKey(1000);
 	system("pause");
+
     return 0;
 }
---
+++ ConvolutionalNN/README.txt
@@ -0,0 +1,6 @@
+Dodaæ do zmiennych œrodowiskowych:
+
+zmienna 	= wartoœæ
+----------------------------------
+OPENCV_DIR 	= (opencv)/build/[x86|x64]/vc11
+PATH		= (dodaæ, nie zamieniæ) %OPENCV_DIR%/bin
