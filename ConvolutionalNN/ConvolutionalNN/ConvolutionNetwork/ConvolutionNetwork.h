c31722cbfadb877e1c7a199f685157c44da7c3c3
Xeverus <michal.wendel@gmail.com>
2014-05-14 19:11:47 +0200

    kolejny fragment implementacji sieci konwolucyjnej

--- ConvolutionalNN/ConvolutionalNN/ConvolutionNetwork/ConvolutionNetwork.h
+++ ConvolutionalNN/ConvolutionalNN/ConvolutionNetwork/ConvolutionNetwork.h
@@ -15,24 +15,30 @@ class ConvolutionNetwork {
 public:
 	ConvolutionNetwork(
 		std::vector<ConvolutionLayer<Input>::PtrS> const&	pLayers,
-		gpu::Converter<Input, Output>::PtrS const&			pConverter);
+		gpu::GpuBuffer::PtrS const&							pOutputBuffer,
+		gpu::Converter<Input, Output>::PtrS const&			pConverter		= gpu::Converter<Input, Output>::PtrS(new gpu::Packer<Input, Output>()));
 	virtual ~ConvolutionNetwork();
 
 	virtual void run();
+
+	virtual gpu::GpuBuffer::PtrS const& getOutputBuffer() const;
 
 
 private:
 	std::vector<ConvolutionLayer::PtrS> mLayers;
 	gpu::Converter<Input, Output>		mConverter;
+	gpu::GpuBuffer::PtrS				mOutputBuffer;
 };
 
 
 template<typename Input, typename Output>
 ConvolutionNetwork<Input, Output>::ConvolutionNetwork(
 	std::vector<ConvolutionLayer<Input>::PtrS> const&	pLayers,
+	gpu::GpuBuffer::PtrS const&							pOutputBuffer,
 	gpu::Converter<Input, Output>::PtrS const&			pConverter)
 :
 	mLayers(pLayers),
+	mOutputBuffer(pOutputBuffer),
 	mConverter(pConverter)
 {
 
@@ -50,12 +56,20 @@ void ConvolutionNetwork<Input, Output>::run(){
 	for (ConvolutionLayer<Input> const& layer : mLayers){
 		(*layer)();
 	}
-	if (mConverter != nullptr){
+	if (mConverter != nullptr && mOutputBuffer != nullptr){
 		mConverter.convert(
 			mLayers[mLayers.size() - 1]->getOutputBatch(),
 			mLayers[mLayers.size() - 1]->getOutputBuffer(),
-			nullptr);
+			mOutputBuffer);
 	}
+}
+
+
+template<typename Input, typename Output>
+gpu::GpuBuffer::PtrS const& ConvolutionNetwork<Input, Output>::getOutputBuffer() const {
+	return mOutputBuffer == nullptr ? 
+		mLayers[mLayers.size() - 1]->getOutputBuffer() :
+		mOutputBuffer;
 }
 
 
